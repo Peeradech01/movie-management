b92186f026cb57276325594ac342efcfbe2f203a
Peeradech01 <suapeeradech@gmail.com>
2026-05-17 21:13:13 +0700

    setup types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,23 @@
+export type UserRole = 'MANAGER' | 'TEAMLEADER' | 'FLOORSTAFF';
+
+export type MovieRating = 'G' | 'PG' | 'M' | 'MA' | 'R';
+
+export interface User {
+  userId: number;
+  username: string;
+  role: UserRole;
+}
+
+export interface Movie {
+  id: number;
+  title: string;
+  yearReleased: number;
+  rating: MovieRating;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface LoginCredentials {
+  username: string;
+  password: string;
+}
